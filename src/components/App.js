85964280ee0f53c3f4ffbcaa81264cc9fdc97a4d
Sam <samshrestha85@gmail.com>
2020-07-26 17:44:58 +0545

    working with routes and components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
+import { Route, Link, Switch } from "react-router-dom";
+
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
 
 export default class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <Switch>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </Switch>
+      </div>
+    );
   }
 }
