bec9d1fa9298722a2e05a73dd25e2d7654d47942
Sam <samshrestha85@gmail.com>
2020-07-28 02:43:17 +0545

    Added list of streams using action creators and reducers

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-function StreamList() {
-  return <div>Stream List</div>;
+import { fetchStreams } from "../../actions/index";
+
+class StreamList extends Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+  render() {
+    // console.log(this.props.streams);
+    const { streams } = this.props;
+    const streamlist = streams.map((stream) => (
+      <div className="item" key={stream.id}>
+        <i className="large middle aligned icon camera" />
+        <div className="content">
+          {stream.title}
+          <div className="description">{stream.description}</div>
+        </div>
+      </div>
+    ));
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{streamlist}</div>
+      </div>
+    );
+  }
 }
 
-export default StreamList;
+function mapStateToProps(state) {
+  // console.log(state);
+  //Object.values()= it's gonna take objects as an arguement and pulled out all the objects and inserted into an array
+  return { streams: Object.values(state.streams) };
+}
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
