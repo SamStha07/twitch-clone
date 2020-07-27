6c300b43cddd12857410f26db1c3a5165bc9e504
Sam <samshrestha85@gmail.com>
2020-07-28 00:16:05 +0545

    Finally our stream create stores data in api as db with REST Conventions

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers/index";
 
-const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
