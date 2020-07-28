1e30055b8693ef947fd83b04bb8f7e21d255c7fb
Sam <samshrestha85@gmail.com>
2020-07-29 01:23:06 +0545

    Added edit navigation which shows the title

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default function StreamEdit() {
-  return <div>Stream Edit</div>;
+import { fetchStream } from "../../actions/index";
+
+class StreamEdit extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    // console.log(this.props);
+    // console.log(props.match.params.id);
+    // console.log(this.props.stream);
+    const { stream } = this.props;
+    //if we don't us if statement, it will throw error like cannot read property
+    if (!stream) {
+      return <div>Loading...</div>;
+    }
+    return <div>{stream.title}</div>;
+  }
 }
+
+//ownProps will use the values form StreamEdit(props)
+function mapStateToProps(state, ownProps) {
+  // console.log(ownProps);
+  return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
