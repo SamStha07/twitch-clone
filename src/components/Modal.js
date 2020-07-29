f047fe19356a07c8ddc6f73b1173aab569ec8ae8
Sam <samshrestha85@gmail.com>
2020-07-30 00:18:07 +0545

    Making Modal reusable and using react fragment

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-export default function Modal() {
+import history from "../history";
+
+export default function Modal(props) {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">asdasdasfsdfsdfdsf</div>
+    <div
+      onClick={() => history.push("/")}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
+      </div>
     </div>,
     document.getElementById("modal")
   );
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -3,10 +3,20 @@ import React from "react";
 import Modal from "../Modal";
 
 const StreamDelete = () => {
+  const actions = (
+    <>
+      <button className="ui negative button">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  );
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+      />
     </div>
   );
 };
