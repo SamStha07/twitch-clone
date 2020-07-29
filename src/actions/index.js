16ced667d20d8a104eb6390c1ba202a50bc8702b
Sam <samshrestha85@gmail.com>
2020-07-29 20:10:12 +0545

    Working with streamEdit with form submission and put vs patch in action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -36,7 +36,7 @@ export const createStream = (formValues) => {
     });
     //Do some programmatic navigation to
     //get the user back to the root route
-    history.push('/');
+    history.push("/");
   };
 };
 
@@ -64,11 +64,12 @@ export const fetchStream = (id) => {
 
 export const editStream = (id, formValues) => {
   return async function (dispatch) {
-    const response = await streams.put(`/streams/${id}`, formValues);
+    const response = await streams.patch(`/streams/${id}`, formValues);
     dispatch({
       type: EDIT_STREAM,
       payload: response.data,
     });
+    history.push("/");
   };
 };
 
--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,22 +1,40 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import _ from "lodash";
 
-import { fetchStream } from "../../actions/index";
+import { fetchStream, editStream } from "../../actions/index";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+
+  onSubmit = (formValues) => {
+    // console.log(formValues);
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
   render() {
-    // console.log(this.props);
-    // console.log(props.match.params.id);
-    console.log(this.props.stream);
+    // console.log(this.props.stream);
     const { stream } = this.props;
     //if we don't us if statement, it will throw error like cannot read property
     if (!stream) {
       return <div>Loading...</div>;
     }
-    return <div>{stream.title}</div>;
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          // initialValues={{
+          //   title: stream.title,
+          //   description: stream.description,
+          // }} //special property from redux form will have {title, description} inside
+          initialValues={_.pick(stream, "title", "description")} //will just the pick the values we need
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   }
 }
 
@@ -26,4 +44,6 @@ function mapStateToProps(state, ownProps) {
   return { stream: state.streams[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
