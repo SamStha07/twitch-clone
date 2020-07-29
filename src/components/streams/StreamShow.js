06e0c713fb4f2204655f50bb980150665fda96d3
Sam <samshrestha85@gmail.com>
2020-07-30 01:40:22 +0545

    Showing title and descp in StreamShow page

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default function StreamShow() {
-  return <div>Stream Show</div>;
+import { fetchStream } from "../../actions/index";
+
+class StreamShow extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    // console.log(this.props.stream);
+    const stream = this.props.stream;
+    if (!this.props.stream) {
+      return <div>Loading....</div>;
+    }
+    return (
+      <div>
+        <h4>Title: {stream.title}</h4>
+        <p>Description: {stream.description}</p>
+      </div>
+    );
+  }
 }
+
+function mapStateToProps(state, ownProps) {
+  return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
