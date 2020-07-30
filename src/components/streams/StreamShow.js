dc406f9cfd6d23c5bebebb1b8fed0d218b2a2926
Sam <samshrestha85@gmail.com>
2020-07-30 15:08:13 +0545

    Added video player using videoref

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import flv from "flv.js";
 
 import { fetchStream } from "../../actions/index";
 
 class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
@@ -15,6 +21,7 @@ class StreamShow extends Component {
     }
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls={true} />
         <h4>Title: {stream.title}</h4>
         <p>Description: {stream.description}</p>
       </div>
